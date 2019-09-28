38bbd150e8b95c1bbd49cfcfa4358aaf9e87fb11
Heramb <44063354+Hearmb@users.noreply.github.com>
2019-09-28 11:44:44 +0530

    footer

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -10,7 +10,8 @@
 
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/navdesign.css">
-    
+    <link rel="stylesheet" href="css/footerdesign.css">
+<
 
 
     <link href="http://allfont.net/allfont.css?fonts=comic-sans-ms" rel="stylesheet" type="text/css" />
@@ -27,8 +28,26 @@
     </ul>
 </div>
 </nav>
+
 @yield('content')
 
+<footer>
+    <div class="footcontent1">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    </div>
+    <div class="footcontent2">
+        <h4 class="connectheader">Connect</h4>
+        <ul class="footerul">
+            <li class="footerlist"><a href="#">Link1</a></li>
+            <li class="footerlist"><a href="#">Link2</a></li>
+            <li class="footerlist"><a href="#">Link3</a></li>
+        </ul>
+
+
+    </div>
+    
+</footer>
 </body>
 </html>
 
