ae5e93be24cf6283c5556a41316923abc8cfa30b
Heramb <44063354+Hearmb@users.noreply.github.com>
2019-09-28 10:37:33 +0530

    nav

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -8,6 +8,27 @@
 </head>
 <body>
 @yield('content')
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Inventions</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">ML</a></li>
+          <li><a href="#">AI</a></li>
+          <li><a href="#">Blockchain</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Question And Answer</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
 </body>
 </html>
 
