f467bab8553e7d2a591fa2f55241092bf50dbe42
try-no-cry <39919696+try-no-cry@users.noreply.github.com>
2019-09-28 11:58:57 +0530

    hero

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,12 @@
 
    <div class="hero">
        <div class="heroimg">
+            <img src="images/hero image.jpg" alt="Hero Image" >
         </div>
         <div class="heroinfo">
         
-        <p> Find all IT related updates<br> at one location <br>Your Fingertips</p>
+            <h1> IT UPDATES </h1>
+            <p> Find all IT related updates<br> at one location <br>Your Fingertips</p>
     
         </div>
    </div>
@@ -18,6 +20,23 @@
     <section  class="flex-div" id="latest">
 
         <h1>LATEST</h1>
+        <div class="cards">
+                <div class="card">
+                    <h3 class="card-head"> Heading 1</h3>
+                    <p class="card-desc"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat sapiente natus, minus itaque quae dignissimos, dicta, ab mollitia ipsum saepe tempore veritatis similique obcaecati magnam tempora ea iure vitae.</p>
+                </div>
+                <div class="card">
+                    <h3 class="card-head"> Heading 2</h3>
+                    <p class="card-desc"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat sapiente natus, minus itaque quae dignissimos, dicta, ab mollitia ipsum saepe tempore veritatis similique obcaecati magnam tempora ea iure vitae.</p>
+                </div><div class="card">
+                    <h3 class="card-head"> Heading 3</h3>
+                    <p class="card-desc"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat sapiente natus, minus itaque quae dignissimos, dicta, ab mollitia ipsum saepe tempore veritatis similique obcaecati magnam tempora ea iure vitae.</p>
+                </div><div class="card">
+                    <h3 class="card-head"> Heading 4</h3>
+                    <p class="card-desc"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat sapiente natus, minus itaque quae dignissimos, dicta, ab mollitia ipsum saepe tempore veritatis similique obcaecati magnam tempora ea iure vitae.</p>
+                </div>
+
+        </div>
     
     </section>
     
