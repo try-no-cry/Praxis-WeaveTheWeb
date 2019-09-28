1c0457d0a19310122596f7a9156799f8715c32b0
Heramb <44063354+Hearmb@users.noreply.github.com>
2019-09-28 11:12:34 +0530

    navbar

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,36 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>IT UPDATES</title>
 
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/navdesign.css">
+    
 </head>
 <body>
-@yield('content')
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Inventions</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
-        <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">ML</a></li>
-          <li><a href="#">AI</a></li>
-          <li><a href="#">Blockchain</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Question And Answer</a></li>
-      <li><a href="#">Page 3</a></li>
+<nav class="homepagenav">
+ <div>
+     <ul class="navcontents">
+        <li class="navlist"> <a href="">Inventions</a></li>
+        <li class="navlist"><a href="#">Page 1-1</a></li>
+        <li class="navlist"><a href="#">Page 1-2</a></li>
+        <li class="navlist"><a href="#">Page 1-3</a></li>
     </ul>
-  </div>
+</div>
 </nav>
+@yield('content')
+
 </body>
 </html>
 
