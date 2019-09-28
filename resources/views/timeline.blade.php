2c30ee2af1bb62fd40cc4f3fd18ba5c77c18b037
Heramb <44063354+Hearmb@users.noreply.github.com>
2019-09-28 13:49:37 +0530

    timeline

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+@extends('layout.navbar')
+
+@section('content')
+
+
 <style>
 * {
   box-sizing: border-box;
 }
 
-body {
-  background-color:rgb(255, 251, 197);
-  font-family: Helvetica, sans-serif;
-}
+
 
 /* The actual timeline (the vertical ruler) */
 .timeline {
@@ -25,10 +23,10 @@ body {
   content: '';
   position: absolute;
   width: 6px;
-  background-color: white;
+  background-color: lightblue;
   top: 0;
   bottom: 0;
-  left: 50%;
+  left: 54%;
   margin-left: -3px;
 }
 
@@ -47,7 +45,7 @@ body {
   width: 25px;
   height: 25px;
   right: -17px;
-  background-color: white;
+  background-color:#5edfff;
   border: 4px solid #FF9F55;
   top: 15px;
   border-radius: 50%;
@@ -100,7 +98,7 @@ body {
 /* The actual content */
 .content {
   padding: 20px 30px;
-  background-color: white;
+  background-color: #5edfff;
   position: relative;
   border-radius: 6px;
   text-align: center;
@@ -168,8 +166,7 @@ body {
  
 }
 </style>
-</head>
-<body>
+
     
         <h1><CENTER>ARTIFICIAL INTELLIGENCE</CENTER></h1>
 
@@ -177,14 +174,14 @@ body {
   <div class="container left">
     <div class="content">
      <h2 style ="color:#FF9F55">1980–1987</h2 >
-      <img src="1.jpg" alt="Nature" style= "width:50%">
+      <img src="images/1.jpg" alt="Nature" style= "width:50%">
       <p>In 1982, physicist John Hopfield was able to prove that a form of neural network (now called a "Hopfield net") could learn and process information in a completely new way. Around the same time, Geoffrey Hinton and David Rumelhart popularized a method for training neural networks called "backpropagation"</p>
     </div>
   </div>
   <div class="container right">
     <div class="content">
      <h2 style ="color:#FF9F55">1987–1993</h2>
-    <img src="2.jpg" alt="Nature" style= "width:50%">
+    <img src="images/2.jpg" alt="Nature" style= "width:50%">
      <P>By 1991, the impressive list of goals penned in 1981 for Japan's Fifth Generation Project had not been met. Indeed, some of them, like "carry on a casual conversation" had not been met by 2010.As with other AI projects, expectations had run much higher than what was actually possible.
 
         Over 300 AI companies had shutdown, gone bankrupt, or been acquired by the end of 1993, effectively ending the first commercial wave of AI.</P>
@@ -194,7 +191,7 @@ body {
   <div class="container left">
     <div class="content">
      <h2 style ="color:#FF9F55">1993–2011</h2>
-     <img src="3.jpg" alt="Nature" style= "width:50%">
+     <img src="images/3.jpg" alt="Nature" style= "width:50%">
      <p>The field of AI, now more than a half a century old, finally achieved some of its oldest goals. It began to be used successfully throughout the technology industry, although somewhat behind the scenes</p>
   </div>
   </div>
@@ -204,21 +201,21 @@ body {
   <div class="container right">
     <div class="content">
      <h2 style ="color:#FF9F55">DEEP LEARNING</h2>
-     <img src="4.jpg" alt="Nature" style= "width:50%">
+     <img src="images/4.jpg" alt="Nature" style= "width:50%">
       <p>Deep learning is a branch of machine learning that models high level abstractions in data by using a deep graph with many processing layers</p>
     </div>
   </div>
   <div class="container left">
     <div class="content">
      <h2 style ="color:#FF9F55">BIG DATA</h2>
-     <img src="5.jpg" alt="Nature" style= "width:50%">
+     <img src="images/5.jpg" alt="Nature" style= "width:50%">
      <P>Big data refers to a collection of data that cannot be captured, managed, and processed by conventional software tools within a certain time frame.</P>
     </div>
   </div>
   <div class="container right">
     <div class="content">
       <h2 style ="color:#FF9F55">ARTIFICIAL GENERAL INTELLIGENCE</h2>
-      <img src="6.jpg" alt="Nature" style= "width:50%">
+      <img src="images/6.jpg" alt="Nature" style= "width:50%">
       <P>Artificial intelligence is a branch of computer science that attempts to understand the essence of intelligence and produce a new intelligent machine that responds in a manner similar to human intelligence.</P>
     </div>
   </div>
@@ -229,5 +226,5 @@ body {
 
     
 </div>
-</body>
-</html>
+
+@endsection
